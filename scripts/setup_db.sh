202594ed1a5608eaa83e93e53f0a39c43ea5e6c0
Igor Esposito <sposigor@gmail.com>
2026-04-02 11:28:07 -0300

    Initial commit (histórico limpo)

---
+++ scripts/setup_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DB_NAME="db_main"
+DB_USER="postgres"
+
+echo "Criando banco..."
+
+sudo -u postgres psql <<EOF
+CREATE DATABASE $DB_NAME;
+EOF
+
+echo "Importando estrutura..."
+
+psql -U $DB_USER -d $DB_NAME -f database_backup/schema.sql
+
+echo "Banco pronto."
